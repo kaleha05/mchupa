b61bc0d407f32e4243ab563e5d15f05f60afc6b1
kaleha05 <keyagloria@gmail.com>
2021-03-20 12:00:59 +0300

    \

--- pages/connect.php
+++ pages/connect.php
@@ -7,4 +7,17 @@
     if(!$conn){
         die('Could not Connect to MySql:' .mysql_error());
     }
+
+
+/*
+
+    $servername='localhost';
+    $username='princes3_princes3';
+    $password= 'Myprincess12';
+    $dbname = "princes3_princess";
+    $conn=mysqli_connect($servername,$username,$password,"$dbname");
+    if(!$conn){
+        die('Could not Connect to MySql:' .mysql_error());
+    }
+*/
 ?>
